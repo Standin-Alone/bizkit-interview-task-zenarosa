e8a94fd8b13dc12c0d15234ab6d3d87fd09e4af7
Standin-Alone <johnedcelzenarosa@gmail.com>
2023-12-01 20:47:49 +0800

    update match.py

--- phasebook/match.py
+++ phasebook/match.py
@@ -1,6 +1,5 @@
 import time
 from flask import Blueprint
-
 from .data.match_data import MATCHES
 
 
@@ -11,17 +10,18 @@ bp = Blueprint("match", __name__, url_prefix="/match")
 def match(match_id):
     if match_id < 0 or match_id >= len(MATCHES):
         return "Invalid match id", 404
-
+    
     start = time.time()
     msg = "Match found" if (is_match(*MATCHES[match_id])) else "No match"
     end = time.time()
 
-    return {"message": msg, "elapsedTime": end - start}, 200
+    return {"message": msg, "elapsedTime": end - start,"myArray":MATCHES[match_id]}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
-
+def is_match(fave_numbers_1, fave_numbers_2):    
+    sorted_fave_numbers_1 = sorted(fave_numbers_1,key=lambda k:k)
+    sorted_fave_numbers_2 = sorted(fave_numbers_2,key=lambda k:k)
+    for number in sorted_fave_numbers_2:
+        if number not in sorted_fave_numbers_1:
+            return False    
     return True
